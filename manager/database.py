8c99cbd048f7e8f4b767c0d5931fd86169b72eeb
Maciej Siekierski <maciejsiekier@gmail.com>
2017-01-17 22:06:09 +0100

    lock to synchronize access to database

--- manager/database.py
+++ manager/database.py
@@ -2,23 +2,26 @@ import logging
 import json
 import sqlite3
 import datetime
+import threading
 
 class Database:
     logger = logging.getLogger()
 
     def __init__(self, dbFile):
         self.dbFile = dbFile
+        self.lock = threading.Lock()
         
     def save(self, deviceIdHex, sensor, protocol, ip, time, numberValue):
         self.logger.debug("saving to db: device={0}, sensor={1}, protocol={2} ip={3}, time={4}, numberValue={5}".format(deviceIdHex, sensor, protocol, ip, time, numberValue))
         try:
-            conn = sqlite3.connect(self.dbFile)
-            deviceId = self.getDeviceId(conn, deviceIdHex)
-            sensorId = self.getSensorId(conn, sensor)
-            addressId = self.getAddressId(conn, protocol, ip)
-            self.saveReading(conn, deviceId, sensorId, addressId, time, numberValue)
-            conn.commit()
-            conn.close()
+            with self.lock:
+                conn = sqlite3.connect(self.dbFile)
+                deviceId = self.getDeviceId(conn, deviceIdHex)
+                sensorId = self.getSensorId(conn, sensor)
+                addressId = self.getAddressId(conn, protocol, ip)
+                self.saveReading(conn, deviceId, sensorId, addressId, time, numberValue)
+                conn.commit()
+                conn.close()
         except Exception as e:
             self.logger.exception(e)
             
@@ -37,89 +40,91 @@ class Database:
         #.strftime('%Y-%m-%d %H:%M:%S')
         c.execute("INSERT INTO readings(time, year, month, day, hour, minute, deviceId, sensorId, addressId, numberValue) VALUES(?,?,?,?,?,?,?,?,?,?)", (time, time.year, time.month, time.day, time.hour, time.minute, deviceId, sensorId, addressId, numberValue))
         
-    def getChartData(self, sensors, timeFrom, timeTo, aggregation):      
-        timeFormat = "%Y-%m-%d %H:%M"
-        groupingColumns = "year, month, day, hour, minute"
-        intervalSeconds = 60
-        if aggregation == "hour":
-            timeFormat = "%Y-%m-%d %H"
-            groupingColumns = "year, month, day, hour"
-            intervalSeconds = 3600
-        elif aggregation == "day":
-            timeFormat = "%Y-%m-%d"
-            groupingColumns = "year, month, day"  
-            intervalSeconds = 24*3600      
-        
-        conn = sqlite3.connect(self.dbFile)
-        c = conn.cursor()
-        conditions = []
-        for sensor in sensors:
-            conditions.append(" (devices.hex = '{0}' AND sensors.name='{1}') ".format(sensor.deviceId, sensor.sensorId))
-        sourcesCondition = " OR ".join(conditions)
-        query = "SELECT devices.hex, sensors.name, {0}, AVG(numberValue) FROM readings JOIN devices ON readings.deviceId=devices.id JOIN sensors ON readings.sensorId=sensors.id WHERE ({1}) AND readings.time>=? AND readings.time<=? GROUP BY devices.hex, sensors.name, {0} ORDER BY {0}".format(groupingColumns, sourcesCondition)
-        self.logger.debug("Executiong query: {0}".format(query))
-        data = {}
-        dbRowsCount = 0
-        t1 = datetime.datetime.now()
-        for row in c.execute(query, (timeFrom, timeTo)):
-            if dbRowsCount == 0:
-                t2 = datetime.datetime.now()
-                self.logger.debug("First row of data arrived after {0}s".format((t2-t1).total_seconds()))
-            dbRowsCount += 1
-            timeKey = ""
-            valueIdx = 7
-            if aggregation == "day":
-                timeKey = "{0:04d}-{1:02d}-{2:02d}".format(row[2], row[3], row[4])
-                valueIdx = 5
-            elif aggregation == "hour":
-                timeKey = "{0:04d}-{1:02d}-{2:02d} {3:02d}".format(row[2], row[3], row[4], row[5])
-                valueIdx = 6
-            else:
-                timeKey = "{0:04d}-{1:02d}-{2:02d} {3:02d}:{4:02d}".format(row[2], row[3], row[4], row[5], row[6])
+    def getChartData(self, sensors, timeFrom, timeTo, aggregation):     
+        with self.lock:    
+            timeFormat = "%Y-%m-%d %H:%M"
+            groupingColumns = "year, month, day, hour, minute"
+            intervalSeconds = 60
+            if aggregation == "hour":
+                timeFormat = "%Y-%m-%d %H"
+                groupingColumns = "year, month, day, hour"
+                intervalSeconds = 3600
+            elif aggregation == "day":
+                timeFormat = "%Y-%m-%d"
+                groupingColumns = "year, month, day"  
+                intervalSeconds = 24*3600      
+            
+            conn = sqlite3.connect(self.dbFile)
+            c = conn.cursor()
+            conditions = []
+            for sensor in sensors:
+                conditions.append(" (devices.hex = '{0}' AND sensors.name='{1}') ".format(sensor.deviceId, sensor.sensorId))
+            sourcesCondition = " OR ".join(conditions)
+            query = "SELECT devices.hex, sensors.name, {0}, AVG(numberValue) FROM readings JOIN devices ON readings.deviceId=devices.id JOIN sensors ON readings.sensorId=sensors.id WHERE ({1}) AND readings.time>=? AND readings.time<=? GROUP BY devices.hex, sensors.name, {0} ORDER BY {0}".format(groupingColumns, sourcesCondition)
+            self.logger.debug("Executiong query: {0}".format(query))
+            data = {}
+            dbRowsCount = 0
+            t1 = datetime.datetime.now()
+            for row in c.execute(query, (timeFrom, timeTo)):
+                if dbRowsCount == 0:
+                    t2 = datetime.datetime.now()
+                    self.logger.debug("First row of data arrived after {0}s".format((t2-t1).total_seconds()))
+                dbRowsCount += 1
+                timeKey = ""
                 valueIdx = 7
-            fields = {"time": timeKey, "numberValue": row[valueIdx], "deviceId":row[0], "sensorId":row[1]}
-            source = "{0}.{1}".format(fields["deviceId"], fields["sensorId"])
-            if not fields["time"] in data:
-                data[fields["time"]] = {}
-            data[fields["time"]][source] = {"numberValue":fields["numberValue"]}  
-        if len(data) == 0:
-            return []
-        minTimeStr = min(data.items(),key=lambda x: x[0])[0]
-        maxTimeStr = max(data.items(),key=lambda x: x[0])[0]
-        self.logger.debug("{0} raw data rows loaded from db and processed to give {1} time keys for period {2} - {3}".format(dbRowsCount, len(data), minTimeStr, maxTimeStr))
-        minTime = datetime.datetime.strptime(minTimeStr, timeFormat)
-        maxTime = datetime.datetime.strptime(maxTimeStr, timeFormat)
-        timeKeysCount = 0
-        emptyCount = 0
-        chartData = []
-        for singleTime in self.daterange(minTime, maxTime, intervalSeconds):
-            timeKeysCount += 1
-            timeKey = singleTime.strftime(timeFormat)
-            if timeKey not in data:
-                data[timeKey] = {}
-                emptyCount += 1
-            data[timeKey]["time"] = timeKey
-            chartData.append(data[timeKey])  
-        self.logger.debug("There are {0} time points in period {1} - {2}. {3} empty time points added.".format(timeKeysCount, minTimeStr, maxTimeStr, emptyCount))
-        conn.close()
-        return chartData
+                if aggregation == "day":
+                    timeKey = "{0:04d}-{1:02d}-{2:02d}".format(row[2], row[3], row[4])
+                    valueIdx = 5
+                elif aggregation == "hour":
+                    timeKey = "{0:04d}-{1:02d}-{2:02d} {3:02d}".format(row[2], row[3], row[4], row[5])
+                    valueIdx = 6
+                else:
+                    timeKey = "{0:04d}-{1:02d}-{2:02d} {3:02d}:{4:02d}".format(row[2], row[3], row[4], row[5], row[6])
+                    valueIdx = 7
+                fields = {"time": timeKey, "numberValue": row[valueIdx], "deviceId":row[0], "sensorId":row[1]}
+                source = "{0}.{1}".format(fields["deviceId"], fields["sensorId"])
+                if not fields["time"] in data:
+                    data[fields["time"]] = {}
+                data[fields["time"]][source] = {"numberValue":fields["numberValue"]}  
+            if len(data) == 0:
+                return []
+            minTimeStr = min(data.items(),key=lambda x: x[0])[0]
+            maxTimeStr = max(data.items(),key=lambda x: x[0])[0]
+            self.logger.debug("{0} raw data rows loaded from db and processed to give {1} time keys for period {2} - {3}".format(dbRowsCount, len(data), minTimeStr, maxTimeStr))
+            minTime = datetime.datetime.strptime(minTimeStr, timeFormat)
+            maxTime = datetime.datetime.strptime(maxTimeStr, timeFormat)
+            timeKeysCount = 0
+            emptyCount = 0
+            chartData = []
+            for singleTime in self.daterange(minTime, maxTime, intervalSeconds):
+                timeKeysCount += 1
+                timeKey = singleTime.strftime(timeFormat)
+                if timeKey not in data:
+                    data[timeKey] = {}
+                    emptyCount += 1
+                data[timeKey]["time"] = timeKey
+                chartData.append(data[timeKey])  
+            self.logger.debug("There are {0} time points in period {1} - {2}. {3} empty time points added.".format(timeKeysCount, minTimeStr, maxTimeStr, emptyCount))
+            conn.close()
+            return chartData
         
     def getDevicesData(self):
-        conn = sqlite3.connect(self.dbFile)
-        c = conn.cursor()
-        devicesData = {}
-        for row in c.execute("SELECT devices.id, devices.hex, max(readings.time) FROM devices JOIN readings ON devices.id = readings.deviceId GROUP BY devices.id, devices.hex"):
-            did = row[0]
-            devDict = {"deviceId":str(row[1].encode('UTF-8')), "lastContact":row[2]}
-            devDict["values"] = {}
-            c2 = conn.cursor()
-            for sensor in c2.execute("SELECT sensors.name, readings.numberValue, addresses.ip, addresses.protocol FROM readings JOIN sensors ON readings.sensorId = sensors.id JOIN addresses ON readings.addressId = addresses.id WHERE readings.time=? AND deviceId=?", (devDict["lastContact"],did)):
-                devDict["address"] = sensor[2]
-                devDict["protocol"] = sensor[3]
-                devDict["values"][sensor[0]] = sensor[1]
-            devicesData[devDict["deviceId"]] = devDict           
-        conn.close()
-        return devicesData
+        with self.lock:    
+            conn = sqlite3.connect(self.dbFile)
+            c = conn.cursor()
+            devicesData = {}
+            for row in c.execute("SELECT devices.id, devices.hex, max(readings.time) FROM devices JOIN readings ON devices.id = readings.deviceId GROUP BY devices.id, devices.hex"):
+                did = row[0]
+                devDict = {"deviceId":str(row[1].encode('UTF-8')), "lastContact":row[2]}
+                devDict["values"] = {}
+                c2 = conn.cursor()
+                for sensor in c2.execute("SELECT sensors.name, readings.numberValue, addresses.ip, addresses.protocol FROM readings JOIN sensors ON readings.sensorId = sensors.id JOIN addresses ON readings.addressId = addresses.id WHERE readings.time=? AND deviceId=?", (devDict["lastContact"],did)):
+                    devDict["address"] = sensor[2]
+                    devDict["protocol"] = sensor[3]
+                    devDict["values"][sensor[0]] = sensor[1]
+                devicesData[devDict["deviceId"]] = devDict           
+            conn.close()
+            return devicesData
             
     def daterange(self, startDate, endDate, intervalSeconds):
         for n in range(int ((endDate - startDate).total_seconds() / intervalSeconds) + 2):
